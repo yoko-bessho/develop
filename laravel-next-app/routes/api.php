f8decf109b39c5b37162699b556a09608fe6e654
yoko bessho <yoko.murakami.11.18@gmail.com>
2026-06-23 07:23:16 +0900

    route追加もれ

---
+++ laravel-next-app/routes/api.php
@@ -0,0 +1,6 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
